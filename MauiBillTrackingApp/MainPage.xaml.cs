86f6f4f1db8db0416a98369b6ecdc32f23fb39ea
tonyk <tonyk@TONY>
2025-05-27 21:54:59 +0300

    get bill list from database working

--- MauiBillTrackingApp/MainPage.xaml.cs
+++ MauiBillTrackingApp/MainPage.xaml.cs
@@ -1,24 +1,58 @@
-﻿namespace MauiBillTrackingApp
+﻿using BillTrackingAppBackend.Models;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
+namespace MauiBillTrackingApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
+            LoadDataFromRestAPI();
+        }
+        public async Task LoadDataFromRestAPI()
+        {
+            try
+            {
+                Loading_label.IsVisible = true;
+
+                HttpClientHandler handler = new HttpClientHandler
+                {
+                    ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+                };
+
+                HttpClient client = new HttpClient(handler)
+                {
+                    BaseAddress = new Uri("https://10.0.2.2:7234/")
+                };
+
+                string json = await client.GetStringAsync("api/BillTrackings");
+
+                IEnumerable<BillTracking> ienumBillList = JsonConvert.DeserializeObject<BillTracking[]>(json);
+                ObservableCollection<BillTracking> BillList = new ObservableCollection<BillTracking>(ienumBillList);
+
+                billList.ItemsSource = BillList;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+            finally
+            {
+                Loading_label.IsVisible = false;
+            }
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void billList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            count++;
+            BillTracking? selectedItem = billList.SelectedItem as BillTracking;
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void selectBill_Clicked(object sender, EventArgs e)
+        {
+            BillTracking? selected = billList.SelectedItem as BillTracking;
         }
     }
 
---
+++ MauiBillTrackingApp/Models/BillTracking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BillTrackingAppBackend.Models;
+
+public partial class BillTracking
+{
+    public int BillId { get; set; }
+
+    public string Lähettäjä { get; set; } = null!;
+
+    public decimal Summa { get; set; }
+
+    public DateTime Eräpäivä { get; set; }
+
+    public string Tila { get; set; } = null!;
+
+    public DateTime? Maksupäivä { get; set; }
+}
